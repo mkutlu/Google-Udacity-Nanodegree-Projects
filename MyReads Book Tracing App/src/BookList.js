f13a84de14610a560ab26b683771797fff02e247
Mesut Kutlu <mkutlu@etu.edu.tr>
2018-08-28 21:21:55 +0300

    api get all books and show proper section

---
+++ MyReads Book Tracing App/src/BookList.js
@@ -0,0 +1,62 @@
+import React from 'react'
+// import * as BooksAPI from './BooksAPI'
+import './App.css'
+import { Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Book from './Book'
+
+class BookList extends React.Component {
+  state = {
+
+  }
+
+  render() {
+    const currentlyReading = this.props.books.filter((book) => book.shelf === 'currentlyReading');
+    const read = this.props.books.filter((book) => book.shelf === 'read');
+    const wantToRead = this.props.books.filter((book) => book.shelf === 'wantToRead')
+    return (
+     <div className="list-books">
+        <span>{currentlyReading.length}</span>
+          
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <Book list={currentlyReading} />
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Wants to Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <Book list={wantToRead} />
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <Book list={read} />
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to='/search'>Add a Book</Link>                                                                                                                                                      
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BookList
+
+
